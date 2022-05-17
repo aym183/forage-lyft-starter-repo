29049083c3a6d36fa7b8a9ca0de80ed1f2005a79
Ayman <ayman.ali1302@gmail.com>
2022-05-17 13:36:25 +0530

    Added diff engine type specifications

--- engine/sternman_engine.py
+++ engine/sternman_engine.py
@@ -1,14 +1,17 @@
 from abc import ABC
-
 from car import Car
+from engine.engine import Engine
 
 
-class SternmanEngine(Car, ABC):
+class SternmanEngine(Engine, ABC):
     def __init__(self, warning_light_is_on):
         self.warning_light_is_on = warning_light_is_on
 
-    def engine_should_be_serviced(self):
-        if self.warning_light_is_on:
-            return True
-        else:
-            return False
+    def needs_service(self):
+        pass
+
+    # def engine_should_be_serviced(self):
+    #     if self.warning_light_is_on:
+    #         return True
+    #     else:
+    #         return False
--- engine/willoughby_engine.py
+++ engine/willoughby_engine.py
@@ -1,6 +1,6 @@
 from abc import ABC
-
 from car import Car
+from engine.engine import Engine
 
 
 class WilloughbyEngine(ABC):
@@ -8,5 +8,7 @@ class WilloughbyEngine(ABC):
         self.current_mileage = current_mileage
         self.last_service_mileage = last_service_mileage
 
-    def engine_should_be_serviced(self):
-        return self.current_mileage - self.last_service_mileage > 60000
+    def needs_service(self):
+        pass
+    # def engine_should_be_serviced(self):
+    #     return self.current_mileage - self.last_service_mileage > 60000
