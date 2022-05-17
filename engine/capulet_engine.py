e927d8702a0557f513107a0e3c2e78ff0442236a
Ayman <ayman.ali1302@gmail.com>
2022-05-17 12:52:31 +0530

    Added classes and methods for batteries

--- engine/capulet_engine.py
+++ engine/capulet_engine.py
@@ -3,7 +3,7 @@ from abc import ABC
 from car import Car
 
 
-class CapuletEngine(Car, ABC):
+class CapuletEngine(ABC):
     def __init__(self, last_service_date, current_mileage, last_service_mileage):
         super().__init__(last_service_date)
         self.current_mileage = current_mileage
