6eb73a90986fe2eeca94b1fbf75b57be6b36fc90
Ayman <ayman.ali1302@gmail.com>
2022-05-17 12:44:53 +0530

    Added battery and model directories

--- car.py
+++ car.py
@@ -2,9 +2,10 @@ from abc import ABC, abstractmethod
 
 
 class Car(ABC):
-    def __init__(self, last_service_date):
-        self.last_service_date = last_service_date
 
-    @abstractmethod
+    def __init__(self, engine, battery):
+        self.engine = engine
+        self.battery = battery
+
     def needs_service(self):
         pass
