6a5e943439764026d97fef998cc3b3a2f2e6cf48
Ayman <ayman.ali1302@gmail.com>
2022-05-17 15:37:20 +0530

    Added battery and engine types folders

---
+++ engine/engine_types/sternman_engine.py
@@ -0,0 +1,17 @@
+from abc import ABC
+from car import Car
+from engine.engine import Engine
+
+
+class SternmanEngine(Engine, ABC):
+    def __init__(self, warning_light_is_on):
+        self.warning_light_is_on = warning_light_is_on
+
+    def needs_service(self):
+        pass
+
+    # def engine_should_be_serviced(self):
+    #     if self.warning_light_is_on:
+    #         return True
+    #     else:
+    #         return False
---
+++ engine/engine_types/willoughby_engine.py
@@ -0,0 +1,14 @@
+from abc import ABC
+from car import Car
+from engine.engine import Engine
+
+
+class WilloughbyEngine(Engine, ABC):
+    def __init__(self, current_mileage, last_service_mileage):
+        self.current_mileage = current_mileage
+        self.last_service_mileage = last_service_mileage
+
+    def needs_service(self):
+        pass
+    # def engine_should_be_serviced(self):
+    #     return self.current_mileage - self.last_service_mileage > 60000
