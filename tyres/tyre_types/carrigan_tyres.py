ea85b373c84b90dace0d07ede02be26e776a52c5
Ayman <ayman.ali1302@gmail.com>
2022-05-18 08:54:03 +0530

    Added servicing constraints for tyres

---
+++ tyres/tyre_types/carrigan_tyres.py
@@ -0,0 +1,15 @@
+from abc import ABC
+from tyres.tyres import Tyre
+
+class CarriganTyres(Tyre, ABC):
+    def __init__(self, tyres_array):
+        self.tyres_array = tyres_array
+
+    def tyre_needs_service(self):
+        for i in self.tyres_array:
+            if i < 0 or i > 1:
+                return "Invalid"
+            elif i>= 0.9:
+                return True
+            else:
+                return False
